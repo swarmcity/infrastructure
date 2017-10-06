a82d0d68d253850039fea80261b8fca2bccd68f6
@bkawk <email.will.in.china@gmail.com>
2017-10-07 00:59:45 +0800

    ec recover and tooling

--- Site/dragon.js
+++ Site/dragon.js
@@ -1,5 +1,5 @@
 
-import { sha3, privateToAddress,  privateToPublic, publicToAddress, toChecksumAddress, ecsign } from 'ethereumjs-util';
+import { sha3, privateToAddress,  privateToPublic, publicToAddress, toChecksumAddress, ecsign, ecrecover, toBuffer, bufferToHex, fromRpcSig } from 'ethereumjs-util';
 import { createHash, randomBytes, createCipheriv, pbkdf2Sync, createDecipheriv } from 'crypto';
 import scrypt from 'scryptsy';
 import uuid from 'uuid';
@@ -14,6 +14,10 @@ export {
   publicToAddress,
   toChecksumAddress,
   ecsign,
+  ecrecover,
+  toBuffer,
+  bufferToHex,
+  fromRpcSig,
   createHash, 
   randomBytes, 
   createCipheriv, 
