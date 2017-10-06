d17bccaf878dd8af2622ce53135b3d14047f18a7
@bkawk <email.will.in.china@gmail.com>
2017-10-06 09:33:33 +0800

    connect parity socket

--- API/socket.js
+++ API/socket.js
@@ -1,25 +1,33 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const Web3 = require('web3');
 
+const web3 = new Web3.providers.WebsocketProvider("ws://localhost:8546");
+const minimeContract_abi = require('./contracts/MiniMeToken.json');
+var minimeContract = new web3.eth.Contract(minimeContract_abi.abi, '0xb9e7f8568e08d5659f5d29c4997173d84cdf2607');
+
+console.log(minimeContract.methods.symbol().call());
+console.log(minimeContract.methods.balanceOf('0x7018d8f698bfa076e1bdc916e2c64caddc750944').call());
+
+io.on('connect', (socket) => {
+    socket.emit('connected');
+    socket.on('subscribe', (data, fn) => {
+		switch (data.channel) {
+            case 'balance':
+                console.log('balance')
+				//balanceSubscribeHandler(socket, data, fn);
+				break;
+            case 'hashtags':
+                //hashtagsSubscribeHandler(socket, data, fn);
+                break;
+			default:
+				console.log('unknown channel', data.channel);
+				break;
+		}
+    });
+});
 
 const PORT = 8011;
 const HOST = '0.0.0.0';
 server.listen(PORT, HOST);
-
-
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
-});
-
-io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
-});
-
-io.on('connect', (socket) => {
-    require('./handlers/connected')(socket);
-    require('./handlers/subscribe')(socket);
-});
