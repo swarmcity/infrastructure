93b5afdb2b6482967607b86ca37c772501486440
@bkawk <email.will.in.china@gmail.com>
2017-10-11 20:24:38 +0800

    queue framework

--- API/socket.js
+++ API/socket.js
@@ -1,65 +1,136 @@
+"use strict";
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const Web3 = require('web3');
-
-function printMultipleMessages(error, result) {
-    for (resultIndex in result) {
-        printSingleMessage(error, result[resultIndex]);
+let queue = [];
+/**
+ * Start
+ */
+(function(){
+    // get time in miliseconds
+    const timeNow = (new Date).getTime();
+    //push get FX to the queue
+    const getFx = {
+        lastRun: timeNow,
+        nextRun: timeNow,
+        interval: 300000,
+        toDo: '_getFx',
+        error: '_errorFx',
+        publicKey: 0,
     }
-}
-function printSingleMessage(error, result) {
-    try {
-        if (!error) {
-            console.log(result);
-        } else {
-            console.log(error);
-        }
-    } catch (e) {
-        console.log(e);
+    _queue(getFx, 'add');
+    //push get Hashtags to the queue
+    const getHashtags = {
+        lastRun: timeNow,
+        nextRun: timeNow,
+        interval: 300000,
+        toDo: '_getHashtags',
+        error: '_errorHashtags',
+        publicKey: 0,
     }
-}
+    _queue(getHashtags, 'add');
+    //push get Gas Price to the queue
+    const getGasPrice = {
+        lastRun: timeNow,
+        nextRun: timeNow,
+        interval: 300000,
+        toDo: '_getGasPrice',
+        error: '_errorGasPrice',
+        publicKey: 0,
+    }
+    _queue(getGasPrice, 'add');
+    //push get Health to the queue
+    const getHealth = {
+        lastRun: timeNow,
+        nextRun: timeNow,
+        interval: 300000,
+        toDo: '_getHealth',
+        error: '_errorHealth',
+        publicKey: 0,
+    }
+    _queue(getHealth, 'add');
+    // start the queue manager
+    setTimeout(function(){ 
+        _queue(getHealth, 'remove');
+     }, 3000);
+    _queueManager();
+})();
 
-// const tokenAddress = "0xb9e7f8568e08d5659f5d29c4997173d84cdf2607";
-// const web3Hostname = 'ws://localhost:8546';
-// const tokenAbi = [{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_amount","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Approval","type":"event"}];
-
-// var web3 = new Web3(Web3.givenProvider || web3Hostname);
-// var bittrexAddress = '0xFBb1b73C4f0BDa4f67dcA266ce6Ef42f520fBB98';
-
-// var myContract = new web3.eth.Contract(tokenAbi, tokenAddress);
-// console.log("contract options: " + myContract.options)
-
-// console.log("balance of bittrex: ");
-// myContract.methods.balanceOf(bittrexAddress).call(printSingleMessage);
-
-
+/**
+ * Connect
+ */
 io.on('connect', (socket) => {
-    socket.emit('connected');
-    socket.on('subscribe', (data, fn) => {
-		switch (data.channel) {
-            case 'balance':
-                console.log('balance')
-				// balanceSubscribeHandler(socket, data, fn);
-				break;
-            case 'hashtags':
-                //hashtagsSubscribeHandler(socket, data, fn);
-                break;
-			default:
-				console.log('unknown channel', data.channel);
-				break;
-		}
+    //push get balance to the queue
+    console.log('connected')
+    /**
+     * Disconnect
+     */
+    socket.on('disconnect', (socket) => {
+        // Remove all the items from the queue for this socket
+        console.log('disconnected')
+    });
+    /**
+     * One Time Requests
+     */
+    socket.on('requests', (socket) => {
+        console.log('requests')
+    });
+    socket.on('broadcastTransaction', (socket) => {
+        console.log('broadcastTransaction')
+    });
+    socket.on('saveAvatar', (socket) => {
+        console.log('saveAvatar')
+    });
+    socket.on('getNoonce', (socket) => {
+        console.log('getNoonce')
+    });
+    socket.on('saveError', (socket) => {
+        console.log('saveError')
     });
 });
 
-// function balanceSubscribeHandler(socket, data, fn) {
-//
-//     fn({
-//         success: true,	// http 200 here..
-//         balance: balance, //.toString(10),
-//         subscription: 'aaaaa'
-//     });
-// }
+// the queue will need add, remove and return, not update!
+// direction is 'add' or 'remove'
+// if the direction is remove then a public key and toDo must be provided
+
+function _queue(task, direction) {
+    if(task && direction == 'add'){
+        queue.push(task);
+        // make a log
+    } else if (task && direction == 'remove'){
+        queue = queue.filter(function(obj) {
+            return (obj.publicKey !== task.publicKey || obj.toDo !== task.toDo); // Double santiy check this the || feels wrong!!
+        });
+        // make a log
+    } else {
+        return queue;
+    }
+    console.log(queue)
+}
+
+function _queueManager() {
+    console.log('queueManager')
+    // start checking the queue every second for items with a next_run time less than or equal to the time now
+    const queue = _queue();
+    const timeNow = (new Date).getTime();
+}
+
+function _blockWatcher() {
+    console.log('blockWatcher')
+    // start checking for a new block
+    // if you find a new block get the balance for each connected user
+}
+function _eventLog(item, type) {
+    console.log(item, type)
+    // start checking for a new block
+    // if you find a new block get the balance for each connected user
+}
+function _errorLog(item, type) {
+    console.log(item, type)
+    // start checking for a new block
+    // if you find a new block get the balance for each connected user
+}
 
 const PORT = 8011;
 const HOST = '0.0.0.0';
