16593b17c4540d6c897cc934011ddab0eb29cc59
@bkawk <email.will.in.china@gmail.com>
2017-10-11 22:58:51 +0800

    more comments

--- API/socket.js
+++ API/socket.js
@@ -118,25 +118,22 @@ function _queueManager() {
         });
         _taskSheduler(tasksToDo);
     }, 1000);
-
 }
 
-
-function _taskSheduler(tasksToDo) {
-// make an array and only push ino items that are not a duplicate from whats already in 
-// make a promise loop
-// one after another call the function in the task, passing the task itself into the function
-}
-
+/** 
+* Watch the blockchain for a new block
+*/
 function _blockWatcher() {
     setInterval(() => { 
         // ask parity if a new block has been born
+        // if one was found set block found to true
         let blockFound = false;
+        let getBalanceFor = [];
         if(blockFound == true){
             // loop over all connected users
             // for each user push the below into an array
             // make log
-            
+
             // const getBalance = {
             //     lastRun: 0,
             //     nextRun: 0,
@@ -146,11 +143,46 @@ function _blockWatcher() {
             // }
 
             // When done send the array to the task scheduler
-            _taskSheduler(getBalance);
+            _taskSheduler(getBalanceFor);
         }
     }, 3000);
     // if you find a new block get the balance for each connected user
 }
+
+
+/** 
+* Inserts jobs into the job schedular array as they arrive
+* he scheduled jobs are processed as fast as possible, one after another
+* Once a job has been completed its removed from the queue or rescheduled
+*/
+function _taskSheduler(tasksToDo) {
+// make an array and only push ino items that are not a duplicate from whats already in 
+// make a promise loop
+// one after another call the function in the task, passing the task itself into the function
+}
+
+/**
+* Tasks
+*/
+function _getFx(task){
+    console.log('getFx', task);
+};
+function _getHashtags(task){
+    console.log('getHashtags', task);
+};
+function _getGasPrice(task){
+    console.log('getGasPrice', task);
+};
+function _getHealth(task){
+    console.log('getHealth', task);
+};
+function _getBalance(task){
+    console.log('getBalance', task);
+};
+
+/**
+* Logs
+*/
 function _eventLog(item, type) {
     console.log(item, type)
     // start checking for a new block
@@ -161,14 +193,6 @@ function _errorLog(item, type) {
     // start checking for a new block
     // if you find a new block get the balance for each connected user
 }
-
-/**
-*Tasks
-*/
-function _getFx(){}
-function _getHashtags(){}
-function _getGasPrice(){}
-function _getHealth(){}
 
 
 const PORT = 8011;
