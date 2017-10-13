a85a47c0b3059b9fa93e41d438350b0886082eda
psdev <admin@swarmcity.io>
2017-10-13 08:00:02 -0700

    chain in /mnt, run as daemon

--- Chain/run.sh
+++ Chain/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker run                                                      \
-  -v /tmp/parity/:/root/.local/share/io.parity.ethereum/        \
+docker run -d                                                     \
+  -v /mnt/data/parity/:/root/.local/share/io.parity.ethereum/        \
   -p 8080:8080 -p 8180:8180 -p 8545:8545 -p 8546:8546           \
-    psdev/swarmcitychain:v1.7.2-0                               \
+    parity/parity:v1.7.3                               \
       --ws-interface all --ws-origins all --ws-hosts all        \
       --jsonrpc-hosts all                                       \
       --ui-interface 0.0.0.0 --ui-hosts all --ui-no-validation
