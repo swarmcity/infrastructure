8189f4f2d8fd8d3ed12b535132f61acc195f0b8c
kingflurkel <mikethuy@gmail.com>
2017-10-17 13:08:06 +0200

    catch added

--- API/socket.js
+++ API/socket.js
@@ -189,7 +189,7 @@ function _taskScheduler(tasks){
         return tasks.reduce((chain, task) => {
             return chain.then(() => task.toDo(task))
             .then(val => console.log(val, Date.now()));
-        }, Promise.resolve()).then(() => { taskInProgress = false });
+        }, Promise.resolve()).then(() => { taskInProgress = false }).catch(() => { taskInProgress = false });
     }
 }
 
