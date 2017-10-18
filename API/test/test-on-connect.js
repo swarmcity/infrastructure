13245c04c3e19d3dde955e84d27029ddd5678aba
Stefaan Ponnet <stefaan@ponnet.com>
2017-10-18 21:35:22 +0200

    added test for websocket

---
+++ API/test/test-on-connect.js
@@ -0,0 +1,64 @@
+const should = require('should');
+const io = require('socket.io-client');
+
+
+const socketURL = 'http://localhost:8011?publicKey=0x7018d8f698bfa076e1bdc916e2c64caddc750944';
+
+const options = {
+	transports: ['websocket'],
+	'force new connection': true
+};
+
+describe("Swarm City API socket client", function() {
+
+	let client;
+
+	it('should receive "connect" event right after connect', function(done) {
+		client = io.connect(socketURL, options);
+		client.on('connected', function(data) {
+			console.log('client id=', client.id);
+		});
+
+		let promises = [];
+
+		promises.push(new Promise((resolve, reject) => {
+			client.on('balanceChanged', (data) => {
+				console.log('balanceChanged', data);
+				resolve();
+			});
+		}));
+
+		promises.push(new Promise((resolve, reject) => {
+			client.on('fxChanged', (data) => {
+				console.log('fxChanged', data);
+				resolve();
+			});
+		}));
+
+		promises.push(new Promise((resolve, reject) => {
+			client.on('gasPriceChanged', (data) => {
+				console.log('gasPriceChanged', data);
+				resolve();
+			});
+		}));
+
+		promises.push(new Promise((resolve, reject) => {
+			client.on('hashtagsChanged', (data) => {
+				console.log('hashtagsChanged', data);
+				resolve();
+			});
+		}));
+
+		Promise.all(promises).then(() => {
+			done();
+		});
+
+	});
+
+	after(function(done) {
+		client.close();
+		done();
+	});
+
+
+});
