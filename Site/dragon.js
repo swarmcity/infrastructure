db799b9e2c890ae58e00d4124ac8f63dbe41ed18
@bkawk <email.will.in.china@gmail.com>
2017-10-19 15:34:29 +0800

    more lint

--- Site/dragon.js
+++ Site/dragon.js
@@ -1,11 +1,25 @@
 
-import { sha3, privateToAddress,  privateToPublic, publicToAddress, toChecksumAddress, ecsign, ecrecover, toBuffer, bufferToHex, fromRpcSig } from 'ethereumjs-util';
-import { createHash, randomBytes, createCipheriv, pbkdf2Sync, createDecipheriv } from 'crypto';
+import {
+  sha3,
+  privateToAddress,
+  privateToPublic,
+  publicToAddress,
+  toChecksumAddress,
+  ecsign,
+  ecrecover,
+  toBuffer,
+  bufferToHex,
+  fromRpcSig} from 'ethereumjs-util';
+import {
+  createHash,
+  randomBytes,
+  createCipheriv,
+  pbkdf2Sync,
+  createDecipheriv} from 'crypto';
 import scrypt from 'scryptsy';
 import uuid from 'uuid';
 import Buffer from 'buffer';
 import QRCode from 'qrcode';
-
 
 export {
   sha3,
@@ -18,13 +32,13 @@ export {
   toBuffer,
   bufferToHex,
   fromRpcSig,
-  createHash, 
-  randomBytes, 
-  createCipheriv, 
-  pbkdf2Sync, 
+  createHash,
+  randomBytes,
+  createCipheriv,
+  pbkdf2Sync,
   createDecipheriv,
   scrypt,
   uuid,
   Buffer,
   QRCode,
-}
+};
