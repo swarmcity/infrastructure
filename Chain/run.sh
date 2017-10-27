0329081a73e32229c5268f9bad2cc2cfdc27bf25
psdev <p+psdev@psdev.io>
2017-10-27 18:57:56 -0500

    add geth docker, use stable versions

--- Chain/run.sh
+++ Chain/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker run -d                                                     \
-  -v /mnt/data/parity/:/root/.local/share/io.parity.ethereum/        \
+docker run -d                                                   \
+  -v /mnt/data/parity/:/root/.local/share/io.parity.ethereum/   \
   -p 8080:8080 -p 8180:8180 -p 8545:8545 -p 8546:8546           \
-    parity/parity:v1.7.3                               \
+    parity/parity:stable                                        \
       --ws-interface all --ws-origins all --ws-hosts all        \
       --jsonrpc-hosts all                                       \
       --ui-interface 0.0.0.0 --ui-hosts all --ui-no-validation
