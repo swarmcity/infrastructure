aefc4b464d540e5e06d8567ac5add517604d09da
psdev <admin@swarmcity.io>
2017-10-30 10:13:54 -0500

    script not docker

--- Store/init.sh
+++ Store/init.sh
@@ -2,9 +2,9 @@
 
 export IPFS_PATH=/ipfsrepo
 
-RUN ipfs init
-RUN ipfs config Addresses.API /ip4/127.0.0.1/tcp/5001
+ipfs init
+ipfs config Addresses.API /ip4/127.0.0.1/tcp/5001
 
-RUN ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
-RUN ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials "[\"true\"]"
-RUN ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods "[\"PUT\", \"POST\", \"GET\"]"
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials "[\"true\"]"
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods "[\"PUT\", \"POST\", \"GET\"]"
