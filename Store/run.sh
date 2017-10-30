968ac2dc6ae3319369481458ba153e1cf6b34273
psdev <admin@swarmcity.io>
2017-10-30 09:58:30 -0500

    store ipfs repo in volume

--- Store/run.sh
+++ Store/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker run -d                                                   \
+  -v /ipfsrepo:/ipfsrepo                                        \
   -p 4001:4001 -p 5001:5001 -p 9000:9000                        \
     swarmcity_ipfs
--- Store/server.sh
+++ Store/server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export IPFS_PATH=/ipfsrepo
 ipfs daemon &
 node server.js 
